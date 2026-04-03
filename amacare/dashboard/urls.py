2cd0e2ebc5d22c30a7729707cd18b422a0b637fe
liona8 <lionatok08@gmail.com>
2026-04-04 00:35:58 +0800

    connect

--- amacare/dashboard/urls.py
+++ amacare/dashboard/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="dashboard"),
+    path("settings/", views.settings, name="settings"),
+    path("medication/", views.medication, name="medication"),
+    path("chat/", views.chat, name="chat"),
+    path("mood/", views.mood, name="mood"),
+    path("location/", views.location, name="location"),
 ]
--- amacare/dashboard/views.py
+++ amacare/dashboard/views.py
@@ -5,3 +5,18 @@ def home(request):
     # meds = Medication.objects.all()
     # return render(request, "home.html", {"meds": meds})
     return render(request, "dashboard.html")
+
+def settings(request):
+    return render(request, "settings.html")
+
+def medication(request):
+    return render(request, "medication.html")
+
+def chat(request): 
+    return render(request, "chat.html")
+
+def mood(request):
+    return render(request, "mood.html")
+
+def location(request):
+    return render(request, "location.html")
